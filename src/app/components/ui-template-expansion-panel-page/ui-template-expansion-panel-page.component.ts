78e145b1d33bf6f1241647192088773618c3d089
BJ Anderson <andersonb79@gmail.com>
2024-06-19 02:53:36 -0400

    added more material components

--- src/app/components/ui-template-expansion-panel-page/ui-template-expansion-panel-page.component.ts
+++ src/app/components/ui-template-expansion-panel-page/ui-template-expansion-panel-page.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'ui-ui-template-expansion-panel-page',
   standalone: true,
   styleUrl: './ui-template-expansion-panel-page.component.scss',
   templateUrl: './ui-template-expansion-panel-page.component.html',
-  imports: [],
+  imports: [MatExpansionModule],
 })
-export class UiTemplateExpansionPanelPageComponent {}
+export class UiTemplateExpansionPanelPageComponent {
+  readonly panelOpenState = signal(false);
+}
--- src/app/components/ui-template-grid-list-page/ui-template-grid-list-page.component.ts
+++ src/app/components/ui-template-grid-list-page/ui-template-grid-list-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'ui-ui-template-grid-list-page',
   standalone: true,
   styleUrl: './ui-template-grid-list-page.component.scss',
   templateUrl: './ui-template-grid-list-page.component.html',
-  imports: [],
+  imports: [MatGridListModule],
 })
 export class UiTemplateGridListPageComponent {}
