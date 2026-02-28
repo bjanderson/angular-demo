e0af86fb0065e966abcacb3c0fa28f1f5d382d54
BJ Anderson <andersonb79@gmail.com>
2026-02-28 09:04:38 -0500

    updated config

--- src/mocks/utils.mock.ts
+++ src/mocks/utils.mock.ts
@@ -1,7 +1,26 @@
-export function callsSpy(spy, calledWith) {
+import { ErrorResponse } from '@bjanderson/utils';
+import { Observable, throwError } from 'rxjs';
+
+export const callsSpy = (spy, calledWith: unknown[]): void => {
   if (calledWith?.length > 0) {
     expect(spy).toHaveBeenCalledWith(...calledWith);
   } else {
     expect(spy).toHaveBeenCalled();
   }
-}
+};
+
+export const mockFunction = (
+  obj: unknown,
+  method: string,
+  data: Observable<unknown> | unknown
+): void => {
+  jest.spyOn<any, any>(obj, method).mockImplementationOnce(() => data);
+};
+
+export const mockError = (
+  obj: unknown,
+  method: string,
+  errorFn = () => throwError(() => new ErrorResponse({ errorMessage: 'test error' }))
+): void => {
+  jest.spyOn<any, any>(obj, method).mockImplementationOnce(errorFn);
+};
